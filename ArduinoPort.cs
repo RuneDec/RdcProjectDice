bd0fdd44e5c2b626f84d63c2f89f483d87f2aa7a
RuneDec <120711332+RuneDec@users.noreply.github.com>
2022-12-16 09:52:49 +0100

    Add files via upload

---
+++ ArduinoPort.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+
+namespace DobbelsteenV2
+{
+    internal class ArduinoPort
+    {
+        private SerialPort port;
+        public ArduinoPort(String portNumber, int baudRate)
+        {
+            if (port == null)
+            {
+                port = new SerialPort(portNumber, baudRate); //Set your board COM
+                port.Open();
+            }
+        }
+        public void WriteToPort(String text)
+        {
+            if (port != null && port.IsOpen)
+            {
+                port.Write(text);
+            }
+        }
+        public void ClosePort()
+        {
+            if (port != null && port.IsOpen)
+            {
+                port.Close();
+            }
+        }
+    }
+}
